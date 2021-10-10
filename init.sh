4b8e87c99fc9f8828c0255220eefb5a23741608d
Milos Miljanic <milosmilos96@gmail.com>
2021-10-10 14:29:15 +0200

    Move zsh to .config

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.config/zsh/plugins/
+git clone https://github.com/TheEdgeOfRage/ohmyzsh ~/.config/zsh/oh-my-zsh
+
+git clone https://github.com/MichaelAquilina/zsh-you-should-use ~/.config/zsh/plugins/you-should-use
+git clone https://github.com/TheEdgeOfRage/zsh-autoswitch-virtualenv ~/.config/zsh/plugins/zsh-autoswitch-virtualenv
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.config/zsh/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.config/zsh/plugins/zsh-syntax-highlighting
